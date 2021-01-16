9b86d18bd65d4575b363c48590f04fbaf11a0af1
Martin Wilck <mwilck@suse.com>
2021-01-16 22:41:32 +0100

    monitor: implement uevent handling
    This patch implements handling of events received via NVMe-FC
    (fc_udev_device, detection of FC remote ports with NVMe support)
    and AEN events from persistent discovery controller connections.
    
    For actual discovery, we fork and basically run a "nvme connect-all"
    process with for the newly detected discovery controller.
    The reason for forking discovery tasks is twofold: Firstly, we'd
    otherwise be forced to make all discovery connections sequentially,
    which would be slow, as connecting controllers can block on
    the order of seconds even in successful cases.
    
    Secondly, this allows us to use the some global variables like
    fabrics_cfg and tracked_ctrls in the discovery code path. Without forking,
    we'd have to re-write much more code in fabrics.c. In general,
    the alternative to forking would be creating threads, but the
    that would require a large rewrite of our code base.
    
    A single-threaded server that forks off the actual discovery makes
    most sense at this point.
    
    All options known from "nvme connect-all" can be passed to the discovery
    processes as usual, with the exception of the "peristent" option, which
    is always enabled: The monitor must try to create persistent discovery
    connections in order to monitor them.

--- monitor.c
+++ monitor.c
@@ -17,12 +17,16 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <libudev.h>
 #include <signal.h>
 #include <time.h>
 #include <syslog.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/epoll.h>
 
 #include "util/argconfig.h"
@@ -30,6 +34,7 @@
 #include "common.h"
 #include "nvme.h"
 #include "libnvme.h"
+#include "fabrics.h"
 #include "monitor.h"
 #define LOG_FUNCNAME 1
 #include "event/event.h"
@@ -87,10 +92,17 @@ static int create_udev_monitor(struct udev *udev, struct udev_monitor **pmon)
 }
 
 static sig_atomic_t must_exit;
+static sig_atomic_t got_sigchld;
+static sigset_t orig_sigmask;
 
 static void monitor_int_handler(int sig)
 {
 	must_exit = 1;
+}
+
+static void monitor_chld_handler(int sig)
+{
+	got_sigchld = 1;
 }
 
 static int monitor_init_signals(sigset_t *wait_mask)
@@ -103,31 +115,243 @@ static int monitor_init_signals(sigset_t *wait_mask)
 	 * for events.
 	 */
 	sigfillset(&mask);
-	if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1)
+	if (sigprocmask(SIG_BLOCK, &mask, &orig_sigmask) == -1)
 		return -errno;
 	if (sigaction(SIGTERM, &sa, NULL) == -1)
 		return -errno;
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 		return -errno;
 
+	sa.sa_handler = monitor_chld_handler;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+		return -errno;
+
 	/* signal mask to be used in epoll_pwait() */
 	sigfillset(wait_mask);
 	sigdelset(wait_mask, SIGTERM);
 	sigdelset(wait_mask, SIGINT);
+	sigdelset(wait_mask, SIGCHLD);
 
 	return 0;
 }
 
-static void monitor_handle_udevice(struct udev_device *ud)
+static int child_reset_signals(void)
 {
-	nvme_msg(LOG_INFO, "uevent: %s %s\n",
-		 udev_device_get_action(ud),
-		 udev_device_get_sysname(ud));
+	int err = 0;
+	struct sigaction sa = { .sa_handler = SIG_DFL, };
+
+	if (sigaction(SIGTERM, &sa, NULL) == -1)
+		err = errno;
+	if (sigaction(SIGINT, &sa, NULL) == -1 && !err)
+		err = errno;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1 && !err)
+		err = errno;
+
+	if (sigprocmask(SIG_SETMASK, &orig_sigmask, NULL) == -1 && !err)
+		err = errno;
+
+	if (err)
+		nvme_msg(LOG_ERR, "error resetting signal handlers and mask\n");
+	return -err;
+}
+
+static int monitor_get_fc_uev_props(struct udev_device *ud,
+				    char *traddr, size_t tra_sz,
+				    char *host_traddr, size_t htra_sz)
+{
+	const char *sysname = udev_device_get_sysname(ud);
+	const char *tra = NULL, *host_tra = NULL;
+	bool fc_event_seen = false;
+	struct udev_list_entry *entry;
+
+	entry = udev_device_get_properties_list_entry(ud);
+	if (!entry) {
+		nvme_msg(LOG_NOTICE, "%s: emtpy properties list\n", sysname);
+		return -ENOENT;
+	}
+
+	for (; entry; entry = udev_list_entry_get_next(entry)) {
+		const char *name = udev_list_entry_get_name(entry);
+
+		if (!strcmp(name, "FC_EVENT") &&
+		    !strcmp(udev_list_entry_get_value(entry), "nvmediscovery"))
+				fc_event_seen = true;
+		else if (!strcmp(name, "NVMEFC_HOST_TRADDR"))
+			host_tra = udev_list_entry_get_value(entry);
+		else if (!strcmp(name, "NVMEFC_TRADDR"))
+			tra = udev_list_entry_get_value(entry);
+	}
+	if (!fc_event_seen) {
+		nvme_msg(LOG_DEBUG, "%s: FC_EVENT property missing or unsupported\n",
+		    sysname);
+		return -EINVAL;
+	}
+	if (!tra || !host_tra) {
+		nvme_msg(LOG_WARNING, "%s: transport properties missing\n", sysname);
+		return -EINVAL;
+	}
+
+	if (!memccpy(traddr, tra, '\0', tra_sz) ||
+	    !memccpy(host_traddr, host_tra, '\0', htra_sz)) {
+		nvme_msg(LOG_ERR, "traddr (%zu) or host_traddr (%zu) overflow\n",
+		    strlen(traddr), strlen(host_traddr));
+		return -ENAMETOOLONG;
+	}
+
+	return 0;
+}
+
+static int monitor_discovery(nvme_host_t h, char *transport,
+			     char *traddr, char *host_traddr, char *trsvcid)
+{
+	pid_t pid;
+	int rc;
+	nvme_ctrl_t c;
+	struct nvme_fabrics_config fabrics_cfg = {
+		.tos = -1,
+	};
+
+	pid = fork();
+	if (pid == -1) {
+		nvme_msg(LOG_ERR, "failed to fork discovery task: %m");
+		return -errno;
+	} else if (pid > 0) {
+		nvme_msg(LOG_DEBUG, "started discovery task %ld\n", (long)pid);
+		return 0;
+	}
+
+	child_reset_signals();
+	free_dispatcher(mon_dsp);
+
+	nvme_msg(LOG_NOTICE, "starting discovery\n");
+	c = nvme_create_ctrl(NVME_DISC_SUBSYS_NAME, transport, traddr,
+			     host_traddr, NULL, trsvcid);
+	if (!c) {
+		nvme_msg(LOG_ERR, "Failed to allocate discovery controller\n");
+		exit(-ENOMEM);
+	}
+	rc = nvmf_add_ctrl(h, c, &fabrics_cfg, false);
+	if (rc) {
+		nvme_msg(LOG_ERR, "no controller found\n");
+		rc = errno;
+	} else {
+		rc = nvmf_do_discover(c, &fabrics_cfg, NULL,
+				      mon_cfg.autoconnect,
+				      true, 0);
+		if (rc)
+			rc = errno;
+	}
+	nvme_free_ctrl(c);
+
+	exit(-rc);
+	/* not reached */
+	return rc;
+}
+
+static void monitor_handle_fc_uev(nvme_host_t h, struct udev_device *ud)
+{
+	const char *action = udev_device_get_action(ud);
+	const char *sysname = udev_device_get_sysname(ud);
+	char traddr[NVMF_TRADDR_SIZE], host_traddr[NVMF_TRADDR_SIZE];
+
+	if (strcmp(action, "change") || strcmp(sysname, "fc_udev_device"))
+		return;
+
+	if (monitor_get_fc_uev_props(ud, traddr, sizeof(traddr),
+				     host_traddr, sizeof(host_traddr)))
+		return;
+
+	monitor_discovery(h, "fc", traddr, host_traddr, NULL);
+}
+
+static int monitor_get_nvme_uev_props(struct udev_device *ud,
+				      char *transport, size_t tr_sz,
+				      char *traddr, size_t tra_sz,
+				      char *trsvcid, size_t trs_sz,
+				      char *host_traddr, size_t htra_sz)
+{
+	const char *sysname = udev_device_get_sysname(ud);
+	bool aen_disc = false;
+	struct udev_list_entry *entry;
+
+	entry = udev_device_get_properties_list_entry(ud);
+	if (!entry) {
+		nvme_msg(LOG_NOTICE, "%s: emtpy properties list\n", sysname);
+		return -ENOENT;
+	}
+
+	*transport = *traddr = *trsvcid = *host_traddr = '\0';
+	for (; entry; entry = udev_list_entry_get_next(entry)) {
+		const char *name = udev_list_entry_get_name(entry);
+
+		if (!strcmp(name, "NVME_AEN") &&
+		    !strcmp(udev_list_entry_get_value(entry), "0x70f002"))
+				aen_disc = true;
+		else if (!strcmp(name, "NVME_TRTYPE"))
+			memccpy(transport, udev_list_entry_get_value(entry),
+				'\0', tr_sz);
+		else if (!strcmp(name, "NVME_TRADDR"))
+			memccpy(traddr, udev_list_entry_get_value(entry),
+				'\0', htra_sz);
+		else if (!strcmp(name, "NVME_TRSVCID"))
+			memccpy(trsvcid, udev_list_entry_get_value(entry),
+				'\0', trs_sz);
+		else if (!strcmp(name, "NVME_HOST_TRADDR"))
+			memccpy(host_traddr, udev_list_entry_get_value(entry),
+				'\0', tra_sz);
+	}
+	if (!aen_disc) {
+		nvme_msg(LOG_DEBUG, "%s: not a \"discovery log changed\" AEN, ignoring event\n",
+		    sysname);
+		return -EINVAL;
+	}
+
+	if (!*traddr || !*transport) {
+		nvme_msg(LOG_WARNING, "%s: transport properties missing\n", sysname);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static void monitor_handle_nvme_uev(nvme_host_t h, struct udev_device *ud)
+{
+	char traddr[NVMF_TRADDR_SIZE], host_traddr[NVMF_TRADDR_SIZE];
+	char trsvcid[NVMF_TRSVCID_SIZE], transport[5];
+
+	if (strcmp(udev_device_get_action(ud), "change"))
+		return;
+
+	if (monitor_get_nvme_uev_props(ud, transport, sizeof(transport),
+				       traddr, sizeof(traddr),
+				       trsvcid, sizeof(trsvcid),
+				       host_traddr, sizeof(host_traddr)))
+		return;
+
+	monitor_discovery(h, transport, traddr, host_traddr,
+			  strcmp(trsvcid, "none") ? trsvcid : NULL);
+}
+
+static void monitor_handle_udevice(nvme_host_t h, struct udev_device *ud)
+{
+	const char *subsys  = udev_device_get_subsystem(ud);
+
+	if (nvme_log_level >= LOG_INFO) {
+		const char *action = udev_device_get_action(ud);
+		const char *syspath = udev_device_get_syspath(ud);
+
+		nvme_msg(LOG_INFO, "%s %s\n", action, syspath);
+	}
+	if (!strcmp(subsys, "fc"))
+		monitor_handle_fc_uev(h, ud);
+	else if (!strcmp(subsys, "nvme"))
+		monitor_handle_nvme_uev(h, ud);
 }
 
 struct udev_monitor_event {
 	struct event e;
 	struct udev_monitor *monitor;
+	nvme_host_t host;
 };
 
 static int monitor_handle_uevents(struct event *ev,
@@ -141,41 +365,53 @@ static int monitor_handle_uevents(struct event *ev,
 	for (ud = udev_monitor_receive_device(monitor);
 	     ud;
 	     ud = udev_monitor_receive_device(monitor)) {
-		monitor_handle_udevice(ud);
+		monitor_handle_udevice(udev_event->host, ud);
 		udev_device_unref(ud);
 	}
 	return EVENTCB_CONTINUE;
 }
 
-static int monitor_parse_opts(const char *desc, int argc, char **argv)
+static int handle_epoll_err(int errcode)
 {
-	bool quiet = false;
-	bool verbose = false;
-	bool debug = false;
-	bool noauto = false;
-	int ret;
-	OPT_ARGS(opts) = {
-		OPT_FLAG("no-connect",     'N', &noauto,              "dry run, do not autoconnect to discovered controllers"),
-		OPT_FLAG("silent",         'S', &quiet,               "log level: silent"),
-		OPT_FLAG("verbose",        'v', &verbose,             "log level: verbose"),
-		OPT_FLAG("debug",          'D', &debug,               "log level: debug"),
-		OPT_FLAG("timestamps",     't', &nvme_log_timestamp,  "print log timestamps"),
-		OPT_END()
+	if (errcode != -EINTR)
+		return errcode;
+	else if (must_exit) {
+		nvme_msg(LOG_NOTICE, "monitor: exit signal received\n");
+		return ELOOP_QUIT;
+	} else if (!got_sigchld) {
+		nvme_msg(LOG_WARNING, "monitor: unexpected interruption, ignoring\n");
+		return ELOOP_CONTINUE;
+	}
+
+	got_sigchld = 0;
+	while (true) {
+		int wstatus;
+		pid_t pid;
+
+		pid = waitpid(-1, &wstatus, WNOHANG);
+		switch(pid) {
+		case -1:
+			if (errno != ECHILD)
+				nvme_msg(LOG_ERR, "error in waitpid: %m\n");
+			goto out;
+		case 0:
+			goto out;
+		default:
+			break;
+		}
+		if (!WIFEXITED(wstatus))
+			nvme_msg(LOG_WARNING, "child %ld didn't exit normally\n",
+			    (long)pid);
+		else if (WEXITSTATUS(wstatus) != 0)
+			nvme_msg(LOG_NOTICE, "child %ld exited with status \"%s\"\n",
+			    (long)pid, strerror(WEXITSTATUS(wstatus)));
+		else
+			nvme_msg(LOG_DEBUG, "child %ld exited normally\n", (long)pid);
 	};
 
-	ret = argconfig_parse(argc, argv, desc, opts);
-	if (ret)
-		return ret;
-	if (quiet)
-		nvme_log_level = LOG_WARNING;
-	if (verbose)
-		nvme_log_level = LOG_INFO;
-	if (debug)
-		nvme_log_level = LOG_DEBUG;
-	if (noauto)
-		mon_cfg.autoconnect = false;
-
-	return ret;
+out:
+	/* tell event_loop() to continue */
+	return ELOOP_CONTINUE;
 }
 
 static DEFINE_CLEANUP_FUNC(cleanup_udevp, struct udev *, udev_unref);
@@ -196,10 +432,53 @@ int aen_monitor(const char *desc, int argc, char **argv)
 	struct udev_monitor *monitor __cleanup__(cleanup_monitorp) = NULL;
 	struct udev_monitor_event udev_event = { .e.fd = -1, };
 	sigset_t wait_mask;
+	bool quiet = false;
+	bool verbose = false;
+	bool debug = false;
+	bool noauto = false;
+	bool matching_only = false;
+	char *hostnqn = NULL;
+	char *hostid = NULL;
+	nvme_root_t r;
+	nvme_host_t h;
+	struct nvme_fabrics_config cfg = {
+		.tos = -1,
+	};
 
-	ret = monitor_parse_opts(desc, argc, argv);
+	OPT_ARGS(opts) = {
+		OPT_FLAG("no-connect",     'N', &noauto,          "dry run, do not autoconnect to discovered controllers"),
+		OPT_LIST("hostnqn",        'q', &hostnqn,         "user-defined hostnqn (if default not used)"),
+		OPT_LIST("hostid",         'I', &hostid,          "user-defined hostid (if default not used)"),
+		OPT_INT("keep-alive-tmo",  'k', &cfg.keep_alive_tmo,  "keep alive timeout period in seconds"),
+		OPT_INT("reconnect-delay", 'c', &cfg.reconnect_delay, "reconnect timeout period in seconds"),
+		OPT_INT("ctrl-loss-tmo",   'l', &cfg.ctrl_loss_tmo,   "controller loss timeout period in seconds"),
+		OPT_INT("tos",             'T', &cfg.tos,             "type of service"),
+		OPT_FLAG("hdr_digest",     'g', &cfg.hdr_digest,      "enable transport protocol header digest (TCP transport)"),
+		OPT_FLAG("data_digest",    'G', &cfg.data_digest,     "enable transport protocol data digest (TCP transport)"),
+		OPT_INT("nr-io-queues",    'i', &cfg.nr_io_queues,    "number of io queues to use (default is core count)"),
+		OPT_INT("nr-write-queues", 'W', &cfg.nr_write_queues, "number of write queues to use (default 0)"),
+		OPT_INT("nr-poll-queues",  'P', &cfg.nr_poll_queues,  "number of poll queues to use (default 0)"),
+		OPT_INT("queue-size",      'Q', &cfg.queue_size,      "number of io queue elements to use (default 128)"),
+		OPT_FLAG("matching",       'm', &matching_only,   "connect only records matching the traddr"),
+		OPT_FLAG("silent",         'S', &quiet,               "log level: silent"),
+		OPT_FLAG("verbose",        'v', &verbose,             "log level: verbose"),
+		OPT_FLAG("debug",          'D', &debug,               "log level: debug"),
+		OPT_FLAG("timestamps",     't', &nvme_log_timestamp,  "print log timestamps"),
+		OPT_END()
+	};
+
+	nvme_log_pid = true;
+	ret = argconfig_parse(argc, argv, desc, opts);
 	if (ret)
-		goto out;
+		return ret;
+	if (quiet)
+		nvme_log_level = LOG_WARNING;
+	if (verbose)
+		nvme_log_level = LOG_INFO;
+	if (debug)
+		nvme_log_level = LOG_DEBUG;
+	if (noauto)
+		mon_cfg.autoconnect = false;
 
 	ret = monitor_init_signals(&wait_mask);
 	if (ret != 0) {
@@ -213,6 +492,18 @@ int aen_monitor(const char *desc, int argc, char **argv)
 		goto out;
 	}
 
+	r = nvme_scan(NULL);
+	if (!cfg.keep_alive_tmo)
+		cfg.keep_alive_tmo = 30;
+	if (!hostnqn)
+		hostnqn = nvmf_hostnqn_from_file();
+	if (!hostid)
+		hostid = nvmf_hostid_from_file();
+	h = nvme_lookup_host(r, hostnqn, hostid);
+	if (!h) {
+		ret = ENOMEM;
+		goto out;
+	}
 	udev = udev_new();
 	if (!udev) {
 		nvme_msg(LOG_ERR, "failed to create udev object: %m\n");
@@ -229,6 +520,7 @@ int aen_monitor(const char *desc, int argc, char **argv)
 	if (udev_event.e.fd == -1)
 		goto out;
 	udev_event.e.cleanup = cleanup_udev_event;
+	udev_event.host = h;
 	udev_event.monitor = monitor;
 	monitor = NULL;
 
@@ -238,7 +530,7 @@ int aen_monitor(const char *desc, int argc, char **argv)
 		goto out;
 	}
 
-	ret = event_loop(mon_dsp, &wait_mask, NULL);
+	ret = event_loop(mon_dsp, &wait_mask, handle_epoll_err);
 
 out:
 	free_dispatcher(mon_dsp);
